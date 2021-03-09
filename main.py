95c8212538edb22501284bedd0ce2b0f7bde3b9f
Md Rakibul Islam <38698778+Rakibul25@users.noreply.github.com>
2021-03-10 04:36:32 +0600

    data type

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-name = input("your name?")
-print(name)
+#fundamental data types
+a = "Hello World"
+b = 20	
+c = 20.5	
+d = 1j
+e = ["apple", "banana", "cherry"]	
+f = ("apple", "banana", "cherry")	
+g = range(6)	
+h = {"name" : "John", "age" : 36}	
+i = {"apple", "banana", "cherry"}	
+j = frozenset({"apple", "banana", "cherry"})
+k = True	
+l = b"Hello"	
+m = bytearray(5)	
+n = memoryview(bytes(5))
+
+print(e)
