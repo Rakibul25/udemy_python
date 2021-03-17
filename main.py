2f2c4660abaf0867265341bb8a14fac0ae78e242
Md Rakibul Islam <38698778+Rakibul25@users.noreply.github.com>
2021-03-17 17:49:09 +0600

    package and import

--- main.py
+++ main.py
@@ -1,25 +1,2 @@
-# def my_function():
-#   print("Hello from a function")
-
-# my_function()
-
-# def my_function(name):
-#   print("your name " + name)
-
-# my_function('Rakib')
-
-# def my_function(name):
-#   return "hello " + name
-
-# name = my_function('Rakib')
-# print(name)
-
-def add(n1,n2):
-  return  n1+n2
-
-
-a = input()
-b = input()
-sum = add(int(a), int(b))
-
-print(sum)
+from package1.innerpack.insub import insubmeth
+insubmeth()
---
+++ package1/innerpack/insub.py
@@ -0,0 +1,2 @@
+def insubmeth():
+    print("in sub")
